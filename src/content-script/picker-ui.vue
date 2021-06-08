3adcf8d13c3253bc723f3c3c16e45ee8fca860db
pjvanwijk <P.J.vanWijk@student.tudelft.nl>
2021-06-08 18:08:42 +0200

    add picker UI vue component

---
+++ src/content-script/picker-ui.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container fixed-top bg-light" id="pickerUI">
+        <div class="row">
+            <h1>Selector Options</h1>
+            <h2>Specificity: {{specificity}}</h2>
+        </div>
+        <div class="row">
+            <h2>Events</h2>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+                    Add listener
+                </button>
+                <div class="dropdown-menu">
+                    <a v-for="event in events" :key="event" class="dropdown-item" href="#">{{event}}</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <button class="btn btn-primary">Done</button>
+            <button class="btn btn-danger">Cancel</button>
+        </div>         
+    </div>
+</template>
+
+<style scoped lang="css" src="bootstrap/dist/css/bootstrap.css"></style>
+
+<style scoped>
+    /* Importing it here doesn't work. It will not be scoped */
+    /* @import 'bootstrap/dist/css/bootstrap.css'; */
+    /* #pickerUI { */
+        /* position: fixed; */
+        /* border-style: solid;
+        border: 2px crimson;
+        top: 0px;
+        right: 0px; */
+    /* } */
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            specificity: 42,
+            events: ['click', 'mouseover', 'mouseexit']
+        };
+    }
+}
+</script>
