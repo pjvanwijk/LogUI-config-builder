c1aaf201bab2dc91d8fe8d55608bed06e9f329c9
pjvanwijk <P.J.vanWijk@student.tudelft.nl>
2021-06-09 07:50:25 +0200

    popup sends message to background script to request data on creation

--- src/popup/popup.vue
+++ src/popup/popup.vue
@@ -25,27 +25,27 @@
 
                 <h2>Browser-wide Events</h2>
                 <div class="form-check form-switch">
-                    <input v-model="browserEvents.eventsWhileScrolling" class="form-check-input" type="checkbox" id="eventsWhileScrolling-switch">
+                    <input v-model="logUIConfig.browserEvents.eventsWhileScrolling" class="form-check-input" type="checkbox" id="eventsWhileScrolling-switch">
                     <label class="form-check-label" for="eventsWhileScrolling-switch">Block Events while Scrolling</label>
                 </div>
                 <div class="form-check form-switch">
-                    <input v-model="browserEvents.URLChanges" class="form-check-input" type="checkbox" id="URLChanges-switch">
+                    <input v-model="logUIConfig.browserEvents.URLChanges" class="form-check-input" type="checkbox" id="URLChanges-switch">
                     <label class="form-check-label" for="URLChanges-switch">URL Changes</label>
                 </div>
                 <div class="form-check form-switch">
-                    <input v-model="browserEvents.contextMenu" class="form-check-input" type="checkbox" id="contextMenu-switch">
+                    <input v-model="logUIConfig.browserEvents.contextMenu" class="form-check-input" type="checkbox" id="contextMenu-switch">
                     <label class="form-check-label" for="contextMenu-switch">Context Menu</label>
                 </div>
                 <div class="form-check form-switch">
-                    <input v-model="browserEvents.pageFocus" class="form-check-input" type="checkbox" id="pageFocus-switch">
+                    <input v-model="logUIConfig.browserEvents.pageFocus" class="form-check-input" type="checkbox" id="pageFocus-switch">
                     <label class="form-check-label" for="pageFocus-switch">Page Focus</label>
                 </div>
                 <div class="form-check form-switch">
-                    <input v-model="browserEvents.trackCursor" class="form-check-input" type="checkbox" id="trackCursor-switch">
+                    <input v-model="logUIConfig.browserEvents.trackCursor" class="form-check-input" type="checkbox" id="trackCursor-switch">
                     <label class="form-check-label" for="trackCursor-switch">Cursor Movements</label>
                 </div>
                 <div class="form-check form-switch">
-                    <input v-model="browserEvents.pageResize" class="form-check-input" type="checkbox" id="pageResize-switch">
+                    <input v-model="logUIConfig.browserEvents.pageResize" class="form-check-input" type="checkbox" id="pageResize-switch">
                     <label class="form-check-label" for="pageResize-switch">Viewport Resizing</label>
                 </div>
 
@@ -65,7 +65,6 @@
 </template>
 
 <script>
-import BrowserEvents from '../model/logui-configuration/browser-events.js';
 import LogUIConfiguration from '../model/logui-configuration/logui-configuration.js';
 import TrackingConfiguration from '../model/tracking-configuration/tracking-configuration.js';
 
@@ -73,36 +72,41 @@ export default {
     name: 'popupApp',
     data() {
       return {
-        browserEvents: new BrowserEvents(),
         logUIConfig: new LogUIConfiguration(),
         trackingConfig: new TrackingConfiguration(),
         port: null
       };
     },
     methods: {
-        activatePicker: function(event) {
+        activatePicker(event) {
             this.port.postMessage({
                 command: 'activatePicker'
             });
         }
     },
+    watch: {
+        logUIConfig: {
+            handler(val) {
+                // Send a config update message on every change
+                this.port.postMessage({
+                    command: 'updateLogUIConfig',
+                    logUIConfig: val
+                });
+            },
+            deep: true
+        }
+    },
     created() {
-        this.port = chrome.runtime.connect({
-            name: 'loguipopup'
-        });
+        // Initiate connection with background script
+        this.port = chrome.runtime.connect({ name: 'loguipopup' });
 
         this.port.postMessage('LogUI popup is now live!');
-
-        chrome.storage.sync.get([
-                'logUIConfig', 
-                'trackingConfig'
-            ], 
-            (res) => {
-                this.browserEvents = res.logUIConfig.browserEvents;
-                this.logUIConfig = res.logUIConfig;
-                this.trackingConfig = res.trackingConfig;
+        this.port.postMessage({ command: 'getLogUIConfig' });
+        this.port.onMessage.addListener((message) => {
+            if (message.logUIConfig) {
+                this.logUIConfig = message.logUIConfig;
             }
-        );
+        });
     }
 }
 </script>
