41b6eae01759778c0f3b79959dbfd78a96417361
pjvanwijk <P.J.vanWijk@student.tudelft.nl>
2021-06-20 17:37:22 +0200

    styling selector editor and add specificity module

--- src/content-script/selector-editor.vue
+++ src/content-script/selector-editor.vue
@@ -1,44 +1,57 @@
 <template>
-    <!-- <iframe> -->
     <div class="container fixed-top bg-light" id="logui-selector-editor-ui">
-            <h1>LogUI Tracking Options</h1>
-            <h3>Name: <input v-model="name"/></h3>
-            <h3>Selector: {{selector}}</h3>
-            <!-- <h3>Specificity: {{specificity}}</h3> -->
+            <h1>LogUI Element Tracking Options</h1>
+            <hr>
+            <h4>Name: <input v-model="name"/></h4>
+            <h4>Selector:</h4><textarea id="logui-selector-editor-textarea" v-model="selector"/>
+            <h4>Specificity:</h4>
             <vue-slider 
                 v-model="selector"
+                :tooltip="'none'"
                 :data="selectors"
                 :adsorb="true"
-                :width="150"
+                :height="20"
             />
-            <h3>Event: {{selectedEvent}}</h3>
-            <!-- <div class="dropdown"> -->
-                <!-- <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"> -->
-                    <!-- Add listener -->
-                <!-- </button> -->
-                <!-- <div class="dropdown-menu"> -->
-                    <!-- <a v-for="event in availableEvents" :key="event" class="dropdown-item" href="#">{{event}}</a> -->
-                <!-- </div> -->
-            <!-- </div> -->
-            <button v-for="event in availableEvents" :key="event" class="btn" @click="selectedEvent = event">{{event}}</button>
+            <h4>Event:</h4>
+            <button 
+                v-for="event in availableEvents" 
+                :key="event" 
+                :class="selectedEvent === event ? 'btn btn-success' : 'btn btn-light' "
+                @click="selectedEvent = event">
+                    {{event}}
+            </button>
+            
+            <hr>
 
             <button class="btn btn-primary" @click="done">Add to tracking config</button>
             <button class="btn btn-danger" @click="cancel">Cancel</button>
-    </div>
-    <!-- </iframe> -->
+        </div>
 </template>
 
-<style scoped lang="css" src="bootstrap/dist/css/bootstrap.css"></style>
 <style scoped>
     #logui-selector-editor-ui {
         padding: 10px;
         margin-top: 10px;
         opacity: 0.95;
     }
+
+    #logui-selector-editor-ui * {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    #logui-selector-editor-textarea {
+        min-width: 100%;
+        height: 100px;
+        resize: vertical;
+    }
+
     #logui-selector-editor-ui > .btn {
         margin-top: 10px;
+        margin-right: 10px;
     }
 </style>
+
+<style scoped lang="css" src="bootstrap/dist/css/bootstrap.css"></style>
 
 <script>
 // import 'bootstrap';
@@ -46,17 +59,22 @@
 import TrackingConfigurationValue from '../model/tracking-configuration/tracking-configuration-value';
 import VueSlider from 'vue-slider-component';
 import 'vue-slider-component/theme/antd.css';
+import { calculate } from 'specificity';
 
 export default {
     data() {
         return {
             selector: this.availableSelectors[this.availableSelectors.length - 1],
             selectors: this.availableSelectors,
-            specificity: 0,
             name: 'tracker',
             selectedEvent: 'click',
             availableEvents: ['click', 'focus']
         };
+    },
+    computed: {
+        specificity() {
+            return calculate(this.selector);
+        }
     },
     methods: {
         done() {
@@ -80,7 +98,7 @@ export default {
     watch: {
         selector: {
             handler(value) {
-                this.$emit("selectorChanged", value);
+                this.$emit('selectorChanged', value);
             }
         }
     },
